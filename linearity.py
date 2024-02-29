054ed06e48a311a4d96b3da560cbc4d306ce59d1
3737cc <151119815+3737cc@users.noreply.github.com>
2024-02-29 16:12:05 +0800

    重写一键式

--- linearity.py
+++ linearity.py
@@ -1,71 +1,77 @@
 import os
 import numpy as np
 from astropy.io import fits
-import cv2
-from astroalign import register
-
+import astroalign as aa
 from bayer import read_fits, bayer_sequence, save_fits_rgb
-from calibrate import flat_calibration, bias_calibration, dark_calibration
+from fits_processor import apply_bilateral_filter
 
 
-def color_image(input_folder, output_folder, flat_folder, dark_folder, bias_folder, target_folder, base_filename='ColorImage'):
-    # 读取校准图像
-    flat_image = flat_calibration(input_folder, flat_folder)
-    dark_image = dark_calibration(input_folder, dark_folder)
-    bias_image = bias_calibration(input_folder, bias_folder)
+def color_image(input_folder, output_folder, flat_folder, dark_folder, bias_folder, target_folder,
+                base_filename='ColorImage'):
+    global img
+    fits_files = [os.path.join(input_folder, file) for file in os.listdir(input_folder) if file.endswith('.fits')]
 
-    # 替换为你的目标图像路径
-    target_filename = target_folder
-    target_data = fits.getdata(target_filename)
+    if not fits_files:
+        print("在指定的文件夹中找不到FITS文件。")
+        return
+
+    # 读取目标图像
+    target_data = fits.getdata(target_folder)
+
+    # 获取目标图像的数据类型
+    target_dtype = target_data.dtype
 
     # 初始化叠加图像
-    stacked_image = np.zeros_like(target_data)
+    stacked_data_list = []
 
     # 遍历文件夹内所有fits文件
     for filename in os.listdir(input_folder):
         if filename.endswith(".fits"):
             # 读取FITS文件数据
             fits_data = read_fits(os.path.join(input_folder, filename))
+            # 双边降噪图像
+            bilateral_data = apply_bilateral_filter(fits_data)
+            # 对齐图像
+            aligned_data, _ = aa.register(bilateral_data, target_data)
 
-            # 降噪
-            denoise_image = cv2.bilateralFilter(fits_data.astype(np.float32), 9, 75, 75)
+            # 将对齐后的图像数据类型转换为与目标图像相同的数据类型
+            aligned_data = aligned_data.astype(target_dtype)
 
-            # 校准
-            calibrated_image = (denoise_image - dark_image) / (flat_image - bias_image)
+            # 将对齐后的图像添加到列表中
+            stacked_data_list.append(aligned_data)
 
-            # 对齐
-            aligned_image, _ = register(calibrated_image, target_data)
+    stacked_data = np.sum(stacked_data_list, axis=0) / len(fits_files)
 
-            # 叠加
-            stacked_image += aligned_image
+    if stacked_data.dtype != np.uint8:
+        img = stacked_data.astype(np.uint8)
 
-            # 解拜尔
-            bayer_result = bayer_sequence(stacked_image)
+    # 解拜尔
+    bayer_result = bayer_sequence(img)
 
-            # 构造新的输出文件名
-            existing_files = [file for file in os.listdir(output_folder) if file.startswith(base_filename)]
-            num_existing_files = len(existing_files)
-            output_filename = f'{base_filename}_{num_existing_files + 1}.fits'
+    # 构造新的输出文件名
+    existing_files = [file for file in os.listdir(output_folder) if file.startswith(base_filename)]
+    num_existing_files = len(existing_files)
+    output_filename = f'{base_filename}_{num_existing_files + 1}.fits'
 
-            # 构建保存路径
-            save_path = os.path.join(output_folder, output_filename)
+    # 构建保存路径
+    save_path = os.path.join(output_folder, output_filename)
 
-            # 将RGB图像数据转换为FITS所需的格式
-            fits_rgb_data = np.transpose(bayer_result, (2, 0, 1))
+    # 将RGB图像数据转换为FITS所需的格式
+    fits_rgb_data = np.transpose(bayer_result, (2, 0, 1))
 
-            # 保存图像
-            save_fits_rgb(fits_rgb_data, save_path)
-            print(f"Saved color image: {save_path}")
+    # 保存图像
+    save_fits_rgb(fits_rgb_data, save_path)
+    print(f"Saved color image: {save_path}")
 
 
-# if __name__ == "__main__":
-#     # 设定参数
-#     input_folder = "/path/to/input/folder"
-#     output_folder = "/path/to/output/folder"
-#     flat_folder = "/path/to/flat/folder"
-#     dark_folder = "/path/to/dark/folder"
-#     bias_folder = "/path/to/bias/folder"
-#     target_folder = "/path/to/target/image.fits"
-#
-#     # 运行程序
-#     color_image(input_folder, output_folder, flat_folder, dark_folder, bias_folder, target_folder)
+if __name__ == "__main__":
+    # 设定参数
+    input_folder = "E:/Image/QHY5III715C/19_22_21"
+    output_folder = "E:/Image/QHY5III715C/1"
+    flat_folder = "E:/Image/QHY5III715C/dark"
+    dark_folder = "E:/Image/QHY5III715C/dark"
+    bias_folder = "E:/Image/QHY5III715C/dark"
+    target_folder = "E:/Image/QHY5III715C/cope.fits"
+
+    # 运行程序
+    color_image(input_folder, output_folder, flat_folder, dark_folder, bias_folder, target_folder)
